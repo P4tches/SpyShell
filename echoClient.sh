219d6a7e92925418414205c7db3100cc73b33169
P4tches <P4tches@users.noreply.github.com>
2016-10-03 15:03:41 -0700

    Added ARP scan

--- echoClient.sh
+++ echoClient.sh
@@ -7,6 +7,7 @@ date >> /tmp/data.bz
 echo `ifconfig wlp1s0 2>/dev/null|awk '/inet addr:/ {print $2}'` >> /tmp/data.bz  #ifconfig wlp1s0| grep 'inet addr:' >> /tmp/data.bz
 arp -na >> /tmp/data.bz
 
+arp-scan --interface=enp2s0 --localnet >> /tmp/data.bz
 echo "===============End File================" >> /tmp/data.bz
 echo "" >> /tmp/data.bz
 cat /tmp/data.bz | nc security.example.com 20801
