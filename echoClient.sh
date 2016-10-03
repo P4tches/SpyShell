e78e62be1cb15f8219a53a06eb45c46432981eed
P4tches <P4tches@users.noreply.github.com>
2016-10-03 13:25:56 -0700

    Create echoClient.sh

---
+++ echoClient.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Server:
+# nc -k -l -p 20801 > out.txt &
+rm /tmp/data.bz
+echo "===============$(hostname)================" > /tmp/data.bz
+date >> /tmp/data.bz
+echo `ifconfig wlp1s0 2>/dev/null|awk '/inet addr:/ {print $2}'` >> /tmp/data.bz  #ifconfig wlp1s0| grep 'inet addr:' >> /tmp/data.bz
+arp -na >> /tmp/data.bz
+
+echo "===============End File================" >> /tmp/data.bz
+echo "" >> /tmp/data.bz
+cat /tmp/data.bz | nc security.hackerpatches.com 20801
