31d15bd21cdaced1d58658c04e0d2915688645da
P4tches <P4tches@users.noreply.github.com>
2016-10-04 19:56:03 -0700

    Redundant Line of Code

--- echoClient.sh
+++ echoClient.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Server:
 # nc -k -l -p 20801 > out.txt &
-rm /tmp/data.bz
 echo "===============$(hostname)================" > /tmp/data.bz
 date >> /tmp/data.bz
 echo `ifconfig wlp1s0 2>/dev/null|awk '/inet addr:/ {print $2}'` >> /tmp/data.bz  #ifconfig wlp1s0| grep 'inet addr:' >> /tmp/data.bz
